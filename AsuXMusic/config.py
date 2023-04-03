0fc8e901f8c05bfbf78ce507ca098cb54b6a7482
Lucifer6789xz <125817448+Lucifer6789xz@users.noreply.github.com>
2023-04-03 08:42:23 +0530

    Update config.py

--- AsuXMusic/config.py
+++ AsuXMusic/config.py
@@ -11,10 +11,10 @@ SESSION_NAME = getenv("SESSION_NAME", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 API_ID = int(getenv("API_ID", "8186557"))
 API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "AbishnoiMF")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Abishnoi_bots")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5938660179").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5938660179").split()))
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Lucifer_Support1")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Lucifer_update1")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6288073823").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "628807382").split()))
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
 
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/31e9ecee16a46575267a4.jpg")
@@ -24,5 +24,5 @@ IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/c3401a572375b569138c3.png")
 IMG_6 = getenv("IMG_6", "https://telegra.ph/file/d8f8fc1de9110b93ca94c.jpg")
 YOUTUBE_IMG_URL = getenv(
-    "YOUTUBE_IMG_URL", "https://te.legra.ph/file/bc5556476395a0c8e109b.jpg"
+    "YOUTUBE_IMG_URL", "https://telegra.ph/file/bc5556476395a0c8e109b.jpg"
 )
